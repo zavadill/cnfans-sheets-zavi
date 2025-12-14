ab63080bd02620a385b3c89351007ea2acca812f
Adam Zavadil <adamzav33@seznam.cz>
2025-12-14 12:08:00 +0100

    GG

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    loader: 'cloudinary',
-    path: 'https://res.cloudinary.com/dguddgiut/image/upload/',
-  },
+
 };
 
 export default nextConfig;
-
